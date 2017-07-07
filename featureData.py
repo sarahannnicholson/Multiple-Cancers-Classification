badcc93bf30c0534ab2f3406dd11a2610beeb1d2
sarahannnicholson <sarah.ann.nicholson@gmail.com>
2017-07-07 15:41:46 -0700

    Added feature plot [labels are incorrect]

--- featureData.py
+++ featureData.py
@@ -4,6 +4,8 @@ import re
 import numpy as np
 import tqdm
 from sklearn import svm
+from sklearn.svm import LinearSVC
+import matplotlib.pyplot as plt
 
 
 class FeatureData(object):
@@ -19,21 +21,38 @@ class FeatureData(object):
             self.number_of_samples = reader[0].split(' ')[0]
             self.number_of_classes = reader[0].split(' ')[1]
             self.classes = reader[1].split(' ')
-            self.Y = reader[2].split(' ')
+            self.Y =  np.array(reader[2].split(' ')).astype(np.float)
 
     def _get_tumor_samples(self, path):
         with open(path, 'r') as inputFile:
             lines = [l.strip().split('	') for l in inputFile.readlines()]
-            data = np.matrix(lines[3:]).T[2:]
+            data = np.matrix(lines[3:]).T
+            self.feature_names = data[1]
+            data = data[2:]
             data = np.delete(data, list(range(1, data.shape[1], 2)), axis=0)
         self.X = data.astype(np.float16)
 
     def _describe(self):
-        print len(self.X)
-        print len(self.Y)
-        print self.number_of_samples
-    	print self.number_of_classes
+        print "\n------ data description -----"
+        print "X len = ", len(self.X)
+        print "Y len = ", len(self.Y)
+        print "# samples = ", self.number_of_samples
+        print "# classes = ", self.number_of_classes
+        print "-----------------------------\n"
 
+def plot_coefficients(classifier, feature_names, top_features=20):
+     coef = classifier.coef_.ravel()
+     top_positive_coefficients = np.argsort(coef)[-top_features:]
+     top_negative_coefficients = np.argsort(coef)[:top_features]
+     top_coefficients = np.hstack([top_negative_coefficients, top_positive_coefficients])
+
+     # create plot
+     plt.figure(figsize=(30, 15))
+     colors = ['#cccccc' if c < 0 else 'teal' for c in coef[top_coefficients]]
+     plt.bar(np.arange(2 * top_features), coef[top_coefficients], color=colors)
+     feature_names = np.array(feature_names)
+     plt.xticks(np.arange(1, 1 + 2 * top_features), feature_names, rotation='vertical', ha='right')
+     plt.savefig("plot.png")
 
 if __name__ == '__main__':
     training = FeatureData('data/Training_res.txt', 'data/Training_cls.txt')
@@ -42,10 +61,11 @@ if __name__ == '__main__':
     training._describe()
     test._describe()
 
-    model = svm.SVC()
+    model = svm.SVC(kernel='linear')
     model.fit(training.X, training.Y)
-
     results = model.predict(test.X)
+
+    plot_coefficients(model, training.feature_names.tolist()[0])
 
     for index, result in enumerate(results):
     	print str(result) + " " + str(test.Y[index])
