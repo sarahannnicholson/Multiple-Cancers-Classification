7c9baa56d788ba950989753265703ae432a22b69
Evan Hildebrandt <ehildebr@uvic.ca>
2017-07-18 13:13:56 -0700

    manual knn

--- Assignment 3/Knn.py
+++ Assignment 3/Knn.py
@@ -1,60 +1,103 @@
 import csv
 import numpy as np
 from sklearn.metrics import accuracy_score
-from sklearn.neighbors import KNeighborsClassifier
+from scipy.spatial.distance import euclidean
+from collections import Counter
+import random
+
+
 
 class Dataset(object):
 	""" Class responsible for interfacing with our data """
 
-	def __init__(self):
+	def __init__(self, datafile, K):
 		self.X = []
 		self.Y = []
 		self.num_of_classes = 0
-
-	def read_data(self, path):
-		with open(path, 'r') as csv_file:
-			reader = csv.DictReader(csv_file)
+		self.K = K
+		with open(datafile, 'r') as data:
+			reader = csv.DictReader(data)
 			for row in reader:
-				del row['Id']
+				self.X.append(map(float, row.values()[1:-1]))
 				self.Y.append(row.pop('Species', None))
-				self.X.append(map(float, row.values()))
-		self.num_of_classes = len(set(self.Y))
+			self.num_of_classes = len(set(self.Y))
+
+	def step1(self):
+		#Get 15 random "Iris-setosa"
+		#Use 10 to train and 5 to test
+		filtered = [i for i, x in enumerate(self.Y) if x == "Iris-setosa"]
+		random.shuffle(filtered)
+		Y =  [self.Y[i] for i in filtered]
+		X = [self.X[i] for i in filtered]
+		self.trainX = X[:10]
+		self.trainY = Y[:10]
+		self.testX = X[-5:]
+		self.testY = Y[-5:]
+
+	def step2(self):
+		#Get 15 random "Iris-setosa" and 15 random "Iris-versicolor"
+		#Use 10 of each to train and 5 to test
+		self.trainX = []
+		self.trainY = []
+		self.testX = []
+		self.testY = []
+		for iris in ['Iris-setosa', 'Iris-versicolor']:
+			filtered = [i for i, x in enumerate(self.Y) if x == iris]
+			random.shuffle(filtered)
+			Y =  [self.Y[i] for i in filtered]
+			X = [self.X[i] for i in filtered]
+			self.trainX.extend(X[:10])
+			self.trainY.extend(Y[:10])
+			self.testX.extend(X[-5:])
+			self.testY.extend(Y[-5:])
+
+	def neighbours(self, test):
+		distance = []
+		for i in range(len(self.trainY)):
+			distance.append(euclidean(self.trainX[i], test))
+
+		return np.argsort(distance)[:self.K]
+
+	def vote(self, n):
+		votes = Counter([self.trainY[i] for i in n])
+		return votes.most_common(1)[0][0]
 
 
-def kNearestNeighbor(X_train, y_train, X_test, predictions, k):
-	# train on the input data
-	train(X_train, y_train)
+	def predict(self):
+		for i in range(len(self.testY)):
+			n = self.neighbours(self.testX[i])
+			print self.testY[i], self.vote(n)
 
-	# loop over all observations
-	for i in range(len(X_test)):
-		predictions.append(predict(X_train, y_train, X_test[i, :], k))
 
 if __name__ == '__main__':
-	train = Dataset()
-	train.read_data("data/train.csv")
+	train = Dataset('data/Iris.csv', 3)
+	
+	train.step1()
+	train.predict()
 	print '======= Train =========='
-	print train.X, '\n'
-	print train.Y, '\n'
+	print train.trainX, '\n'
+	print train.trainY, '\n'
+	print 'classes =', train.num_of_classes
+	print '========================'
+	print '======= Test =========='
+	print train.testX, '\n'
+	print train.testY, '\n'
 	print 'classes =', train.num_of_classes
 	print '========================'
 
-	test = Dataset()
-	test.read_data("data/test.csv")
-	print '======== Test ========='
-	print test.X, '\n'
-	print test.Y, '\n'
-	print 'classes =' , test.num_of_classes
-	print '======================= \n'
+	train.step2()
+	print '======= Train =========='
+	print train.trainX, '\n'
+	print train.trainY, '\n'
+	print 'classes =', train.num_of_classes
+	print '========================'
+	print '======= Test =========='
+	print train.testX, '\n'
+	print train.testY, '\n'
+	print 'classes =', train.num_of_classes
+	print '========================'
+	train.predict()
 
-	# Train model
-	clf = KNeighborsClassifier(len(train.X))
-	clf.fit(train.X, train.Y)
-
-	# Test model
-	pred_Y = clf.predict(test.X)
-
-	# evaluate accuracy
-	print "accuracy = ", accuracy_score(test.Y, pred_Y)
 
 
 
