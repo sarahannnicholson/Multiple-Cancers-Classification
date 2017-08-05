71f5befa94417008fb9fbc0ad3c2ba1b80c18a44
sarahannnicholson <sarah.ann.nicholson@gmail.com>
2017-08-05 12:16:21 -0700

    Added feature selection

--- featureData.py
+++ featureData.py
@@ -1,34 +1,37 @@
-import csv
-import logging
-import re
+#!/usr/bin/env python
 import numpy as np
-from collections import Counter
+from tqdm import tqdm
+import csv, logging, re
 from sklearn.svm import SVC
+from collections import Counter
 from sklearn import preprocessing
 import matplotlib.pyplot as plt
+from sklearn.feature_selection import mutual_info_classif
+from sklearn.feature_selection import SelectKBest
 
 class Data(object):
 	"""Class responsible for interfacing with our data, eg) getting the data, stats, etc.."""
 
-	def __init__(self, res_path, cls_path, feat_elim):
+	def __init__(self, res_path, cls_path, dataType):
+		self.dataType = dataType
 		self._get_classes(cls_path)
-		self._get_tumor_samples(res_path, feat_elim)
+		self._get_tumor_samples(res_path)
 
 	def _get_classes(self, path):
+		print "Getting " + self.dataType + " classes"
 		with open(path, 'r') as f:
-			reader = [l.strip() for l in f.readlines()]
+			reader = [l.strip() for l in tqdm(f.readlines())]
 			self.number_of_samples = reader[0].split(' ')[0]
 			self.number_of_classes = reader[0].split(' ')[1]
 			self.classes = reader[1].split(' ')[0:]
 			self.Y = reader[2].split(' ')
 
-	def _get_tumor_samples(self, path, feat_elim):
+	def _get_tumor_samples(self, path):
+		print "Getting " + self.dataType + " samples"
 		with open(path, 'r') as inputFile:
-			lines = [l.strip().split('	') for l in inputFile.readlines()]
+			lines = [l.strip().split('	') for l in tqdm(inputFile.readlines())]
 			data = np.matrix(lines[3:])
 			self.feature_names = data[:,1]
-			if feat_elim:
-				data = feature_elimination(data)
 			data = data[:,2:]
 			data = np.delete(data, list(range(1, data.shape[1], 2)), axis=1)
 		self.X = data.astype(float).T
@@ -41,14 +44,15 @@ class Data(object):
 			return False
 
 	def _describe(self):
-		print "\n------ data description -----"
+		print "\n------ data " + self.dataType + " description -----"
 		print "X len = ", len(self.X)
 		print "Y len = ", len(self.Y)
 		print "# samples = ", self.number_of_samples
 		print "# classes = ", self.number_of_classes
 		print "-----------------------------\n"
 
-def feature_elimination(data):
+# Failed attempt to do feature selection
+def feature_selection_F(data):
 	new_X = data[0] # hackzz
 	with open('bestFeatures.txt', 'r') as f:
 		lines = [l.strip().split(',') for l in f.readlines()]
@@ -58,6 +62,10 @@ def feature_elimination(data):
 		new_X = np.append(new_X, data[rows[0]], axis=0)
 	print new_X
 	return new_X[1:] # Eww
+
+def feature_selection(X, y):
+	best_indices = SelectKBest(mutual_info_classif, k=2).fit(X, y).get_support(indices=True)
+	return best_indices
 
 def plot_coefficients(classifier, feature_names, class_name, top_features=20):
 	 coef = classifier.coef_[0]
@@ -78,33 +86,45 @@ def plot_coefficients(classifier, feature_names, class_name, top_features=20):
 def run_test(train, test):
 	train._describe()
 	test._describe()
+	doFeatureSelection = False
 
-	for c in test.classes[1:]:
-		trainY = train._get_binary(c)
-		testY = test._get_binary(c)
+	print "Train and test model"
+	for c in tqdm(test.classes[1:]):
+		y_train = train._get_binary(c)
+		y_test  = test._get_binary(c)
 
-		if not trainY or not testY:
+		if not y_train or not y_test:
 			print "Not enough data"
 			continue
+		
+		if doFeatureSelection: 	# Best features
+			best_indices = feature_selection(train.X, y_train)
+			X_train = train.X[:,best_indices]
+			X_test = test.X[:,best_indices]
+		else: #without best features
+			X_train = train.X
+			X_test = test.X
 
 		model = SVC(kernel="linear")
-		model.fit(train.X, trainY)
+		model.fit(X_train, y_train)
 		#plot_coefficients(model, train.feature_names.tolist()[0], c)
-		results = model.predict(test.X)
-		res = zip(results, testY)
+		results = model.predict(X_test)
+		res = zip(results, y_test)
 		truePos = np.count_nonzero([y[0] for y in res if y[1]])
 		falsePos = np.count_nonzero([y[0] for y in res if not y[1]])
 		falseNeg = np.count_nonzero([not y[0] for y in res if y[1]])
 		print c
 		#print float(truePos) / (truePos + falseNeg)
 		print truePos
-		# print "T+" + str(truePos)
-		# print "F+" + str(falsePos)
-		# print "F-" + str(falseNeg)
+		print "T+" + str(truePos)
+		print "F+" + str(falsePos)
+		print "F-" + str(falseNeg)
 
 
 if __name__ == '__main__':
-	train = Data('data/Training_res.txt', 'data/Training_cls.txt', True)
-	test = Data('data/Test_res.txt', 'data/Test_cls.txt', True)
+	logging.basicConfig(level=logging.INFO)
+
+	train = Data('data/Training_res.txt', 'data/Training_cls.txt', 'train')
+	test = Data('data/Test_res.txt', 'data/Test_cls.txt', 'test')
 
 	run_test(train, test)
